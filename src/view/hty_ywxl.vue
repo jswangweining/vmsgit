0a0ecb866f23b48a1b65144750265dff4688b3cd
jswangweining <jswangweining@qq.com>
2017-09-28 18:03:10 +0800

    9.28
    9.28

--- src/view/hty_ywxl.vue
+++ src/view/hty_ywxl.vue
@@ -43,11 +43,9 @@
 
 .w-ywxl-titem1 {
     flex: 1;
-    margin-left: 10px;
-    margin-top: 10px;
-    margin-bottom: 10px;
-    margin-right: 10px;
-    background-color: #fff
+    margin: 10px;
+    background-color: #fff;
+    border: 1px solid #f3f3f3
 }
 
 .w-ywxl-titema {
@@ -80,12 +78,117 @@
     margin-right: 10px;
     margin-top: 10px;
     margin-bottom: 10px;
-
     display: flex;
+    background-color: #fff;
+    border: 1px solid #f3f3f3;
 }
 
 .w-ywxl-titem2item {
     flex: 1;
+}
+
+.w-ywxl-titem3item {
+    display: flex;
+    flex-pack: start;
+    flex-wrap: wrap;
+    align-items: center;
+}
+
+.w-ywxl-titem2itema {
+    width: 50%;
+    // padding: 0 2rem;
+}
+
+.w-ywxl-titem2itemas1 {
+    display: block;
+    font-size: 0.6rem;
+}
+
+.w-ywxl-titem2itemas2 {
+    display: block;
+    font-size: 0.9rem;
+    color: #6c81b3;
+}
+
+.w-ywxl-titem2itemline {
+    width: 1px;
+    margin: 0.5rem;
+    height: 5.5rem;
+    background-color: #f3f3f3;
+    float: left;
+}
+
+.w-ywxl-titem2itemcss3 {
+    width: 4.4rem;
+    height: 4.4rem;
+    border: 1px solid #ff7700;
+    border-radius: 100%;
+    float: left;
+    margin-top: 1rem;
+    position: relative;
+    left: 50%;
+    margin-left: -2rem;
+}
+
+.w-ywxl-titem2itemcss3a {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 4.2rem;
+    height: 4.2rem;
+    margin-top: 0.1rem;
+    margin-left: 0.1rem;
+    border: 1px solid #ff7700;
+    border-radius: 100%;
+    animation: scaleout 1s infinite;
+}
+
+.w-ywxl-titem2itemcss3b {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 4.2rem;
+    height: 4.2rem;
+    margin-top: 0.1rem;
+    margin-left: 0.1rem;
+    border-radius: 100%;
+}
+
+.w-ywxl-titem2itemcss3bs1 {
+    width: 100%;
+    display: inline-block;
+    line-height: 2rem;
+    text-align: center;
+}
+
+.w-ywxl-titem2itemcss3bs2 {
+    width: 100%;
+    display: inline-block;
+    text-align: center;
+    font-size: 1.1rem;
+    color: #ff7700
+}
+
+@keyframes scaleout {
+    0% {
+        transform: scale(1.0)
+    }
+    // 25% {
+    //   transform: scale(1.1);
+    //   opacity: 0.75;
+    // }
+    // 50% {
+    //   transform: scale(1.2);
+    //   opacity: 0.5;
+    // }
+    // 75% {
+    //   transform: scale(1.3);
+    //   opacity: 0.25;
+    // }
+    100% {
+        transform: scale(1.4);
+        opacity: 0;
+    }
 }
 
 .w-ywxl-d {
@@ -96,6 +199,107 @@
     right: 12rem;
     width: auto;
     height: auto;
+    background-color: #fff;
+    border: 1px solid #d7d7d7;
+    display: flex;
+    flex-direction: column;
+}
+
+.w-ywxl-dt {
+    width: 100%;
+    height: 3rem;
+    border-bottom: 1px solid #d7d7d7;
+    display: flex;
+}
+
+.w-ywxl-dt1 {
+    padding-left: 1.5rem;
+    line-height: 3rem;
+}
+
+.w-ywxl-dt2 {
+    flex: 1;
+    padding-left: 1.5rem;
+    line-height: 3rem;
+}
+
+.w-ywxl-dd {
+    flex: 1;
+    display: flex;
+}
+
+.w-ywxl-ddl {
+    width: 11.5rem;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+.w-ywxl-ddla {
+    flex: 1;
+    border-right: 1px solid #d7d7d7;
+    border-bottom: 1px solid #d7d7d7;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    background-color: #fcfcfc;
+}
+
+// .w-ywxl-ddla:hover {
+//   border-right: 1px solid #fff
+// }
+.w-ywxl-ddlac {
+    border-right: 1px solid #fff;
+    background-color: #fff;
+}
+
+.w-ywxl-ddlap1 {
+    font-weight: bold;
+    span {
+        font-size: 0.6rem;
+        font-weight: normal;
+    }
+}
+
+.w-ywxl-ddlap2 {
+    font-size: 1.1rem;
+    color: #ff7700;
+    padding: 0.75rem 0;
+}
+
+.w-ywxl-ddlap3 {
+    color: #999;
+    font-size: 0.6rem;
+}
+
+.w-ywxl-ddr {
+    flex: 1;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+.w-ywxl-ddrt {
+    flex: 1.2;
+    padding: 0 0.5rem;
+    display: flex;
+    flex-direction: column;
+}
+
+.w-ywxl-ddrtd {
+    flex: 1;
+    border-bottom: 1px solid #f3f3f3;
+    display: flex;
+}
+
+.w-ywxl-ddrtditem {
+  flex: 1
+}
+
+.w-ywxl-ddrd {
+    flex: 2;
 }
 
 .w-ywxl-r {
@@ -121,7 +325,7 @@
     &:before {
         content: '';
         position: absolute;
-        left: 0px;
+        left: 0;
         top: 0.75rem;
         display: block;
         width: 0.25rem;
@@ -172,9 +376,8 @@
 }
 
 .w-ywxl-r-ditemcbtn {
-    margin-bottom: 0px;
+    margin-bottom: 0;
     border: 1px solid #fff;
-    ;
     background: none;
     padding: 5px;
     border-radius: 5px;
@@ -235,20 +438,140 @@
         </div>
         <div class="w-ywxl-titem2">
             <div class="w-ywxl-titem2item">
-                <div id="main">
+                <div id="main" style=" height:6rem;">
 
                 </div>
             </div>
             <div class="w-ywxl-titem2item">
-                2
+                <div class="w-ywxl-titem2itemline">
+
+                </div>
+                <div class="w-ywxl-titem2itemcss3">
+
+                    <div class="w-ywxl-titem2itemcss3a">
+
+                    </div>
+                    <div class="w-ywxl-titem2itemcss3b">
+                        <span class="w-ywxl-titem2itemcss3bs1">综合能力</span>
+                        <span class="w-ywxl-titem2itemcss3bs2">6.11</span>
+                    </div>
+                </div>
+
             </div>
-            <div class="w-ywxl-titem2item">
-                3
+            <div class="w-ywxl-titem2item w-ywxl-titem3item">
+                <div class="w-ywxl-titem2itema">
+                    <span class="w-ywxl-titem2itemas1">市盈率（万）</span>
+                    <span class="w-ywxl-titem2itemas2">8.12</span>
+                </div>
+                <div class="w-ywxl-titem2itema">
+                    <span class="w-ywxl-titem2itemas1">市净率（万）</span>
+                    <span class="w-ywxl-titem2itemas2">2.44</span>
+                </div>
+                <div class="w-ywxl-titem2itema">
+                    <span class="w-ywxl-titem2itemas1">市销率（万）</span>
+                    <span class="w-ywxl-titem2itemas2">9.22</span>
+                </div>
+                <div class="w-ywxl-titem2itema">
+                    <span class="w-ywxl-titem2itemas1">会员店价值（万）</span>
+                    <span class="w-ywxl-titem2itemas2">50.2</span>
+                </div>
             </div>
         </div>
     </div>
 
     <div class="w-ywxl-d">
+        <div class="w-ywxl-dt">
+            <div class="w-ywxl-dt1">
+                请选择时间维度
+            </div>
+            <div class="w-ywxl-dt2">
+                <radio-group v-model="radiovalue">
+                    <wradio label="1">当月统计</wradio>
+                    <wradio label="2">全年统计</wradio>
+                </radio-group>
+            </div>
+        </div>
+        <transition name="fade" mode="out-in">
+            <div class="w-ywxl-dd" v-if="radiovalue=='1'" key='a1'>
+                <div class="w-ywxl-ddl">
+                    <div class="w-ywxl-ddla" @click='monthtabs(0,$event)'>
+                        <p class="w-ywxl-ddlap1">总营业收入<span>（万元）</span></p>
+                        <p class="w-ywxl-ddlap2">256</p>
+                        <p class="w-ywxl-ddlap3">同比21%
+                            <svg class="icon" aria-hidden="true">
+                                <use xlink:href="#icon-jiantou1"></use>
+                            </svg>
+                        </p>
+                    </div>
+                    <div class="w-ywxl-ddla" @click='monthtabs(1,$event)'>
+                        <p class="w-ywxl-ddlap1">总销售金额<span>（万元）</span></p>
+                        <p class="w-ywxl-ddlap2">256</p>
+                        <p class="w-ywxl-ddlap3">同比21%
+                            <svg class="icon" aria-hidden="true">
+                                <use xlink:href="#icon-jiantou2"></use>
+                            </svg>
+                        </p>
+                    </div>
+                    <div class="w-ywxl-ddla" style="border-bottom:0" @click='monthtabs(2,$event)'>
+                        <p class="w-ywxl-ddlap1">新增会员总店<span>（家）</span></p>
+                        <p class="w-ywxl-ddlap2">256</p>
+                        <p class="w-ywxl-ddlap3">同比21%
+                            <svg class="icon" aria-hidden="true">
+                                <use xlink:href="#icon-jiantou1"></use>
+                            </svg>
+                        </p>
+                    </div>
+                </div>
+                <transition name="fade" mode="out-in">
+                    <div class="w-ywxl-ddr" v-if="monthtab==0" key='b1'>
+                        <div class="w-ywxl-ddrt">
+                            <div class="w-ywxl-r-t">
+                                总营业收入占比
+                            </div>
+                            <div class="w-ywxl-ddrtd">
+                              <div class="w-ywxl-ddrtditem">
+                                <div id="main2" style=" height:100%">
+
+                                </div>
+                              </div>
+                              <div class="w-ywxl-ddrtditem">
+2
+                              </div>
+                              <div class="w-ywxl-ddrtditem">
+3
+                              </div>
+                              <div class="w-ywxl-ddrtditem">
+4
+                              </div>
+
+                                <!-- <div class="">
+                                  1
+                                </div>
+                                <div class="">
+                                  2
+                                </div> -->
+                            </div>
+                        </div>
+                        <div class="w-ywxl-ddrd">
+                            下
+                        </div>
+
+                    </div>
+                    <div class="w-ywxl-ddr" v-if="monthtab==1" key='b2'>
+                        2
+
+                    </div>
+                    <div class="w-ywxl-ddr" v-if="monthtab==2" key='b3'>
+                        3
+
+                    </div>
+                </transition>
+            </div>
+            <div class="w-ywxl-dd" v-if="radiovalue=='2'" key='a2'>
+                222
+            </div>
+        </transition>
+
 
     </div>
 
@@ -326,114 +649,144 @@
 
 <script>
 
+import $ from 'jquery'
 import echarts from 'echarts'
+import '../assets/fonts/iconfont'
 
 //elementui
 import rate from 'element-ui/packages/rate/src/main.vue'
+import RadioGroup from 'element-ui/packages/radio/src/radio-group.vue'
+import wradio from 'element-ui/packages/radio/src/radio.vue'
 
 export default {
     name: "",
     data: () => ({
-        statvalue: 3.7
+        statvalue: 3.7,
+        radiovalue: '1',
+        monthtab: 0,
+        yeartab: 0
     }),
     mounted() {
         let _this = this;
         this.$nextTick(function() {
             var myChart = echarts.init(document.getElementById('main'));
-
             // 指定图表的配置项和数据
             var option = {
-                // title: {
-                //     text: '自定义雷达图'
-                // },
-                // legend: {
-                //     data: ['图一', '图二', '张三', '李四']
-                // },
                 radar: [{
                     indicator: [{
-                        text: '指标一'
+                        text: '综合能力',
+                        color: '#ff7700'
                     }, {
-                        text: '指标二'
+                        text: '会员店价值'
                     }, {
-                        text: '指标三'
+                        text: '市盈率'
                     }, {
-                        text: '指标四'
+                        text: '市销率'
                     }, {
-                        text: '指标五'
+                        text: '市净率'
                     }],
-                    center: ['25%', '50%'],
-                    radius: 120,
+                    center: ['50%', '50%'],
+                    radius: '40%',
                     startAngle: 90,
                     splitNumber: 4,
-                    shape: 'circle',
                     name: {
-                        formatter: '【{value}】',
+
+                        formatter: '{value}',
                         textStyle: {
-                            color: '#72ACD1'
-                        }
-                    },
-                    splitArea: {
-                        areaStyle: {
-                            color: ['rgba(114, 172, 209, 0.2)',
-                                'rgba(114, 172, 209, 0.4)', 'rgba(114, 172, 209, 0.6)',
-                                'rgba(114, 172, 209, 0.8)', 'rgba(114, 172, 209, 1)'
-                            ],
-                            shadowColor: 'rgba(0, 0, 0, 0.3)',
-                            shadowBlur: 10
-                        }
-                    },
-                    axisLine: {
-                        lineStyle: {
-                            color: 'rgba(255, 255, 255, 0.5)'
-                        }
-                    },
-                    splitLine: {
-                        lineStyle: {
-                            color: 'rgba(255, 255, 255, 0.5)'
+                            color: '#999'
                         }
                     }
                 }],
                 series: [{
-                    name: '雷达图',
                     type: 'radar',
+                    tooltip: {
+                        trigger: 'item'
+                    },
                     itemStyle: {
-                        emphasis: {
-                            // color: 各异,
+                        normal: {
+                            color: "rgba(255,199,1)", // 图表中各个图区域的边框线拐点颜色
                             lineStyle: {
-                                width: 4
+                                color: "#ff7700" // 图表中各个图区域的边框线颜色
+                            },
+                            areaStyle: {
+                                type: 'default',
+                                opacity: 0.5, // 图表中各个图区域的透明度
+                                color: "#ff7700" // 图表中各个图区域的颜色
                             }
                         }
                     },
                     data: [{
-                        value: [100, 8, 0.40, -80, 2000],
-                        name: '图一',
-                        symbol: 'rect',
-                        symbolSize: 5,
-                        lineStyle: {
-                            normal: {
-                                type: 'dashed'
-                            }
-                        }
-                    }, {
-                        value: [60, 5, 0.30, -100, 1500],
-                        name: '图二',
-                        areaStyle: {
-                            normal: {
-                                color: 'rgba(255, 255, 255, 0.5)'
-                            }
-                        }
+                        value: [6.11, 50.2, 8.12, 9.22, 2.44],
+                        name: '综合能力'
                     }]
-                }]
+                }, ]
             }
 
             // 使用刚指定的配置项和数据显示图表。
             myChart.setOption(option);
 
+
+            var myChart2 = echarts.init(document.getElementById('main2'));
+            var option2 = {
+                series: [{
+
+                    type: 'pie',
+                    center: ['50%', '50%'],
+                    radius: '50%',
+                    avoidLabelOverlap: false,
+                    label: {
+                        normal: {
+                            show: false,
+                            position: 'center'
+                        },
+                        // emphasis: {
+                        //     show: true,
+                        //     textStyle: {
+                        //         fontSize: '30',
+                        //         fontWeight: 'bold'
+                        //     }
+                        // }
+                    },
+                    labelLine: {
+                        normal: {
+                            show: false
+                        }
+                    },
+                    data: [{
+                        value: 335,
+                        name: '直接访问'
+                    }, {
+                        value: 310,
+                        name: '邮件营销'
+                    }, {
+                        value: 234,
+                        name: '联盟广告'
+                    }, {
+                        value: 135,
+                        name: '视频广告'
+                    }, {
+                        value: 1548,
+                        name: '搜索引擎'
+                    }]
+                }]
+            };
+            myChart2.setOption(option2);
         })
 
     },
+    methods: {
+        monthtabs: function(index, event) {
+            let _this = this;
+            var el = event.target;
+            $('.w-ywxl-ddlac').removeClass('w-ywxl-ddlac')
+            $(el).hasClass('w-ywxl-ddla') ? $(el).addClass('w-ywxl-ddlac') : $(el).parentsUntil('w-ywxl-ddl').addClass('w-ywxl-ddlac')
+            _this.monthtab = index;
+        }
+    },
     components: {
-        rate
+        rate,
+        RadioGroup,
+        wradio
     }
 }
 
